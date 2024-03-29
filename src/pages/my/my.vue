902fdb5f6b48df1b16ec857a79fe7bd1b37b1bc1
逗逗 <3405608909@qq.com>
2024-03-29 22:27:31 +0800

    feat: tabBar

---
+++ src/pages/my/my.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <view class="my">
+    <view class="i-ant-design:weibo-circle-outlined dou-text-red-500 dou-text-200" />
+  </view>
+</template>
+
+<style scoped lang="scss"></style>
